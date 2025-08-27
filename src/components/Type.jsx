d68a6c725dc9c81b06899a59f9ef0fc99e02aa34
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-08-27 17:22:32 +0530

    Update the Type.jsx file

--- src/components/Type.jsx
+++ src/components/Type.jsx
@@ -9,7 +9,7 @@ function Type() {
           "Full Stack Developer",
           "Software engineer",
           "FullStack Developer",
-          "Open Source Contributor",
+         
         ],
         autoStart: true,
         loop: true,
